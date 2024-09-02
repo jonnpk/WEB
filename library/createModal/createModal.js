e5ae4b59fa3333392965e39794aac27883805dd0
jonnpk <soondae33@gmail.com>
2024-09-02 13:04:02 +0900

    240902

---
+++ library/createModal/createModal.js
@@ -0,0 +1,48 @@
+const modal = document.createElement('div');
+madal.classList.add('modal');
+document.body.appendChild(modal);
+
+const modalWindow = document.createElement('div');
+modalWindow.classList.add('modal-window');
+document.modal.appendChild(modalWindow);
+
+const modalTitle = document.createElement('h2');
+modalTitle.classList.add('modal-title');
+document.modalWindow.appendChild(modalTitle);
+
+const modalText = document.createElement('p');
+modalText.classList.add('modal-text');
+document.modalWindow.appendChild(modalText);
+
+const closeBtn = document.createElement('button');
+closeBtn.classList.add('modal-close');
+closeBtn.innerText = '확 인'
+document.modalWindow.appendChild(closeBtn);
+
+
+closeBtn.addEventListener('click',()=>{
+    closeModal();
+})
+
+window.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('modal')){
+        closeModal();
+    }
+})
+
+window.addEventListener('keydown',(e)=>{
+    if(e.key === 'Escape'){
+        closeModal();
+    }
+})
+
+
+function openModal(title,text){
+    modalTitle.innerText = title;
+    modalText.innerText = text;
+    modal.style.display = 'flex';
+}
+
+function closeModal(){
+    modal.style.display = 'none';
+}
