1c3b9ff77c9723e75177b0c31922b11deecd117a
jonn <soondae33@gmail.com>
2024-09-04 12:56:01 +0900

    240904

--- react/react-project/src/App.js
+++ react/react-project/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Post from './Post'
+import { useRef, useState } from 'react';
 
 function App() {
   const element = <h1 className='title'>안녕하세요 h1입니다.</h1>
@@ -14,10 +15,37 @@ function App() {
       title : '여기 뭐하는 덴가요?',
       text : '...?'
     }]
+  
+  const [number , setNumber] = useState(0)
+  const handleIncressNumber = () =>{
+    setNumber(number+1)
+  }
+  const [text, setText] = useState('')
+  const handleChangeText = (e) =>{
+    setText(e.target.value)
+  }
+
+  const [inputText, setInputText] = useState('')
+  const inputFocus = useRef()
+  const handleChangeTextToButton = () => {
+    setInputText(inputFocus.current.value)
+  }
+
 
   return (
     <div className="App">
+
       {element}
+  
+      <span>{number}</span>
+      <button onClick={handleIncressNumber}>click!</button>
+
+      <input type='text' onChange={handleChangeText}></input>
+      <p>당신의 입력: {text}</p>
+
+      <input type='text' ref={inputFocus}></input>
+      <button onClick={handleChangeTextToButton}>click!!</button>
+      <p>여기에요 여기: {inputText}</p>
 
       {/* <div className='post'>
         <h3>게시물 제목</h3>
@@ -43,6 +71,7 @@ function App() {
       {data.map((value,key)=>
         <Post 제목={value.title} 내용={value.text}></Post>
       )}
+
     </div>
   );
 }
